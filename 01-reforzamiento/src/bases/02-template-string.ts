f148007a27dee968e85b78434fe900117d4e2403
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-09-25 11:58:21 +0200

    TemplateStrings

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,9 @@
+const firstName = "Manuel";
+const lastName = 'R\'omero "Es el apellido de alguien"';
+
+const fullName = `
+${firstName} es el nombre de 
+        alguien
+`;
+
+console.log(firstName, lastName, fullName);
