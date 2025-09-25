fcc3baf3b5ff47a80fb4ed0c083dfa0a8ca993f5
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-09-25 11:17:46 +0200

    Const & let

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,8 @@
+const firstName: string = "Manuel";
+const lastName = "Romero";
+
+let diceNumber = 5;
+diceNumber = 4;
+
+const containsLetter = lastName.includes("R");
+console.log({ containsLetter, diceNumber, firstName, lastName });
