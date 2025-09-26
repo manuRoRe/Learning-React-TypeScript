9b0c473470166e88eb8f28f403239d892b827f4f
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-09-26 18:48:20 +0200

    learning arrow functions

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -2,9 +2,11 @@ function greet(name: string): string {
   return "Hola " + name;
 }
 
-const greet2 = (name: string): string => {
+/* const greet2 = (name: string): string => {
   return "gola " + name;
-};
+}; */
+
+const greet2 = (name: any) => "Hola " + name;
 
 const hello = greet("Manuel");
 const hello2 = greet2("Ant");
@@ -18,5 +20,13 @@ const getUser = () => {
   };
 };
 
+const getUser2 = () => ({ uuid: "ABD-123", username: "Manu61" });
+
 const usuario = getUser();
 console.log(usuario);
+
+const myNumbers: number[] = [1, 2, 3, 4, 5, 6];
+
+myNumbers.forEach((value) => {
+  console.log("valor: " + value);
+});
