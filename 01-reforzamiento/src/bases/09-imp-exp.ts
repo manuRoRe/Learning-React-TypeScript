cae09473622aa81d8f321f4b2e4567e39775ff5b
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-09-29 12:06:47 +0200

    import and export

---
+++ 01-reforzamiento/src/bases/09-imp-exp.ts
@@ -0,0 +1,13 @@
+/* import { heroes as misHeroes,  Hero } from "../data/heroes.data";
+
+const getHeroById = (id: number): Hero | undefined => {
+  const hero = misHeroes.find((hero) => {
+    return hero.id === id;
+  });
+  /* if (!hero) {
+    throw new Error(`Hero with id ${id} not found`);
+  } */
+/*  return hero;
+};
+
+console.log(getHeroById(3)); */
--- 01-reforzamiento/src/data/heroes.data.ts
+++ 01-reforzamiento/src/data/heroes.data.ts
@@ -1,4 +1,4 @@
-interface Hero {
+export interface Hero {
   id: number;
   name: string;
   owner: Owner;
@@ -10,7 +10,7 @@ enum Owner {
   Marvel = "Marvel",
 }
 
-const heroes: Hero[] = [
+export const heroes: Hero[] = [
   {
     id: 1,
     name: "Batman",
@@ -42,3 +42,5 @@ const heroes: Hero[] = [
     owner: Owner.Marvel,
   },
 ];
+
+export default heroes;
