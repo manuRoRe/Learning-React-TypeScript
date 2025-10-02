c807ad8756f342c856ec3b632addb1bbae0fb1fc
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-10-02 10:38:19 +0200

    homework of helpers

--- 02-react-first-steps/src/helpers/math.helper.test.ts
+++ 02-react-first-steps/src/helpers/math.helper.test.ts
@@ -1,14 +1,90 @@
-import { expect, test } from "vitest";
-import { add } from "./math.helper";
+import { describe, expect, test } from "vitest";
+import { add, multiply, subtract } from "./math.helper";
 
-test("should add two positive numbers", () => {
-  //! 1. Arrange
-  const a = 1;
-  const b = 1;
+describe("add", () => {
+  test("should add two positive numbers", () => {
+    //! 1. Arrange
+    const a = 1;
+    const b = 1;
 
-  //! 2. Act
-  const result = add(a, b);
+    //! 2. Act
+    const result = add(a, b);
 
-  //! 3. Assert
-  expect(result).toBe(a + b);
+    //! 3. Assert
+    expect(result).toBe(a + b);
+  });
+  test("should add two positive numbers", () => {
+    //! 1. Arrange
+    const a = 1;
+    const b = 1;
+
+    //! 2. Act
+    const result = add(a, b);
+
+    //! 3. Assert
+    expect(result).toBe(a + b);
+  });
+});
+
+describe("substract", () => {
+  test("should substract two positive numbers", () => {
+    //! 1. Arrange
+    const a = 1;
+    const b = 1;
+
+    //! 2. Act
+    const result = subtract(a, b);
+
+    //! 3. Assert
+    expect(result).toBe(a - b);
+  });
+  test("should substract two negative numbers", () => {
+    //! 1. Arrange
+    const a = -2;
+    const b = -1;
+
+    //! 2. Act
+    const result = subtract(a, b);
+
+    //! 3. Assert
+    expect(result).toBe(a - b);
+  });
+  test("should substract one positive and one negative numbers", () => {
+    //! 1. Arrange
+    const a = -2;
+    const b = 1;
+
+    //! 2. Act
+    const result = subtract(a, b);
+
+    //! 3. Assert
+    expect(result).toBe(a - b);
+  });
+});
+
+describe("multiply", () => {
+  test("should multiply two positive numbers", () => {
+    const a = 5;
+    const b = 5;
+
+    const result = multiply(a, b);
+
+    expect(result).toBe(a * b);
+  });
+  test("should multiply 0 and any number", () => {
+    const a = 5;
+    const b = 0;
+
+    const result = multiply(a, b);
+
+    expect(result).toBe(a * b);
+  });
+  test("should multiply two negative numbers", () => {
+    const a = -5;
+    const b = -5;
+
+    const result = multiply(a, b);
+
+    expect(result).toBe(a * b);
+  });
 });
